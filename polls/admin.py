391e3856e0c752d10de73426a6c941c613d1a2b3
Ebby Wiselyn <ebby@scypho.com>
2016-08-14 18:07:29 +0530

    Register Choice as an option

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from polls.models import Question
+from polls.models import Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
 
 admin.site.register(Question)
+admin.site.register(Choice)
